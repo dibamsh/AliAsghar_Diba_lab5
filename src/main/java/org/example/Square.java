71f73ad73cbd1b51766562f5faf9bcfed49c38ee
dibamsh <45358173+dibamsh@users.noreply.github.com>
2023-05-06 12:09:08 +0330

    Add files via upload

---
+++ src/main/java/org/example/Square.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Square {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double computeArea() {
+        return side * side;
+    }
+}
